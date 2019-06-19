2c17a9519bac0e69a32d6ef90d45023c6aec2ac8
Dennis <qiang.c.chen@accenture.com>
2019-06-19 09:53:08 +0800

    qqq

--- src/routers/rootRouter.ts
+++ src/routers/rootRouter.ts
@@ -7,7 +7,7 @@ export class rootRouter {
   public static create(router: Router) {
     router.get('/test', (req: Request, res: Response, next: NextFunction) => {
       res.writeHead(200, {"Content-Type": "text/plain"});
-      res.send('success');
+      res.end('success');
     });
 
     router.get(
